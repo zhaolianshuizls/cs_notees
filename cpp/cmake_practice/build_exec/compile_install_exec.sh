ee682ecda7950d71445f6cdaff58606c7d49a23f
LianshuiZhao_Mac <zhaolianshui.wzhcz@gmail.com>
2023-10-08 23:07:19 +0800

    CMAKE message levels

--- cpp/cmake_practice/build_exec/compile_install_exec.sh
+++ cpp/cmake_practice/build_exec/compile_install_exec.sh
@@ -1,6 +1,6 @@
 #set -e
 shopt -s extglob
-cmake ../src && make && make install && rm -rf -- !(compile_install_exec.sh)
+cmake ../src --log-level DEBUG && make && make install && rm -rf -- !(compile_install_exec.sh)
 
 cat << EOF > ../install_exec/run_exec.sh
 export DYLD_LIBRARY_PATH=../install_so/
